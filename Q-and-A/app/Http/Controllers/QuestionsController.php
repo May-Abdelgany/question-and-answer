fa9a41ecbbb3e20215306a786fcc99af8c4d8ecf
May-Abdelghany <mayawaad622@gmail.com>
2021-09-05 04:03:24 +0200

    it is controller of question

---
+++ Q-and-A/app/Http/Controllers/QuestionsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Question;
+use Illuminate\Http\Request;
+class QuestionsController extends Controller
+{
+    public function index(){
+        return  Question::all();
+    }
+    public function store(Request $request)
+    {
+        $question=new Question();
+        $question->id=$request->id;
+        $question->title=$request->title;
+        $question->question=$request->question;
+        $question->tag=$request->tag;
+        $question->save();
+       // return new questionResources($question);
+        return $question;
+    }
+
+    public function delete(Question $question){
+        $question=$question->delete();
+        return question::all();
+    }
+}
