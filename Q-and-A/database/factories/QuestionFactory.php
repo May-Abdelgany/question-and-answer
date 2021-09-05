de0fa697bae88968df093c2760da54fdfee7f7db
May-Abdelghany <mayawaad622@gmail.com>
2021-09-05 04:33:14 +0200

    make fake question

---
+++ Q-and-A/database/factories/QuestionFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Question;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class QuestionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Question::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->text(200),
+            'question'=>$this->faker->sentence(100,true),
+            'tag' => $this->faker->text(200),
+        ];
+    }
+}
