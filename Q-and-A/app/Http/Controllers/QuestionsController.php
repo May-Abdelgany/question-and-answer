49877289d9e6738a52a7d4e1f8bfcb6a47b6a0ca
May-Abdelghany <mayawaad622@gmail.com>
2021-09-06 11:57:00 +0200

    update of questions controller

--- Q-and-A/app/Http/Controllers/QuestionsController.php
+++ Q-and-A/app/Http/Controllers/QuestionsController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 use App\Models\Question;
 use Illuminate\Http\Request;
+use App\Http\Requests\Answers_QuestionRequest;
+use App\Http\Resources\Answers_QuestionResource;
+use App\Http\Resources\QuestionResource;
 class QuestionsController extends Controller
 {
     public function index(){
         return  Question::all();
     }
+
+    public function display(/*Answers_Question*/Request $request ,$id)
+        {
+            $question = Answer::with('Question')->where('question_id',$id)->get();
+            //return new Answer_QuestionResource($question);
+           
+            return $question;
+        }
     public function store(Request $request)
     {
         $question=new Question();
@@ -16,9 +27,9 @@ class QuestionsController extends Controller
         $question->question=$request->question;
         $question->tag=$request->tag;
         $question->save();
-        return $question;
+        return new QuestionResource($question);
     }
-
+    
     public function delete(Question $question){
         $question=$question->delete();
         return question::all();
